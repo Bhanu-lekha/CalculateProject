13da63485e9693ca2ea37b0ccd584a247ce5bb68
Bhanu-lekha <bhanulekha2003@gmail.com>
2024-07-01 11:05:26 +0530

    Initial command

---
+++ CalculatorJavaProject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author bhanu
+ *
+ */
+module CalculatorJavaProject {
+}
---
+++ CalculatorJavaProject/src/package1/Calculator.java
@@ -0,0 +1,16 @@
+package package1;
+
+
+public class Calculator {
+	public double findAverage(int num1, int num2, int num3) {
+        double average = (num1 + num2 + num3) / 3.0;
+        return Math.round(average * 100.0) / 100.0;
+    }
+
+    public static void main(String[] args) {
+    	Calculator calculator = new Calculator();
+        double result = calculator.findAverage(13, 9, 16);
+        System.out.println(result);  
+    }
+
+}

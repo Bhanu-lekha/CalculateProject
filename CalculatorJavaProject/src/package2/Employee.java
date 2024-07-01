4eb7bb08fe72866bd2a0b6a0e4581d13ba2693f0
Sumit068 <Sumitpalsingh068@gmail.com>
2024-07-01 13:05:47 +0530

    Eclipse Push Testing

--- CalculatorJavaProject/src/package2/Employee.java
+++ CalculatorJavaProject/src/package2/Employee.java
@@ -1,12 +1,10 @@
 package package2;
 
 public class Employee {
-<<<<<<< HEAD
 
 public double calculateSalary(double basic,double bonus){
-return (basic+bonus);
-=======
+	return (basic+bonus);
 	//testing
->>>>>>> 0fbd0a455e316e9d071703f01148a67c42ec6d43
+}
 	
 }

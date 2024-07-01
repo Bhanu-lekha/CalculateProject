084a94ce199b705d75c10d02f13f86f82547db07
gorantlapranathi <gorantlapranathi77@gmail.com>
2024-07-01 15:02:51 +0530

    "I have added a comment in Calculator.java"

--- CalculatorJavaProject/src/package1/Calculator.java
+++ CalculatorJavaProject/src/package1/Calculator.java
@@ -3,7 +3,7 @@ package package1;
 public class Calculator {
     // calculating avg of 3 numbers
     public double findAverage(int num1, int num2, int num3) {
-
+        //adding a comment
         double average = (num1 + num2 + num3) / 3.0;
         return Math.round(average * 100.0) / 100.0;
     }

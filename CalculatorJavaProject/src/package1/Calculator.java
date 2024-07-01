6ec859065b54050671a87e2b263b0f31eeaf776d
Muskansharma225 <muskan2519sharma@gmail.com>
2024-07-01 15:24:23 +0530

    avg comment

--- CalculatorJavaProject/src/package1/Calculator.java
+++ CalculatorJavaProject/src/package1/Calculator.java
@@ -6,6 +6,7 @@ public class Calculator {
         double average = (num1 + num2 + num3) / 3.0;
         return Math.round(average * 100.0) / 100.0;
     }
+	// avg 
 
     public static void main(String[] args) {
     	Calculator calculator = new Calculator();

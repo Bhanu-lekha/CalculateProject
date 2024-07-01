1a176cd8ce40851976bd7c6035663225079a5ca6
SINGAM BHARGAVI <singambhargvi26@gmail.com>
2024-07-01 12:21:26 +0530

    comment added to calculator.java in package1

--- CalculatorJavaProject/src/package1/Calculator.java
+++ CalculatorJavaProject/src/package1/Calculator.java
@@ -2,6 +2,7 @@ package package1;
 
 
 public class Calculator {
+//Calculating the average of 3 marks
 	public double findAverage(int num1, int num2, int num3) {
         double average = (num1 + num2 + num3) / 3.0;
         return Math.round(average * 100.0) / 100.0;

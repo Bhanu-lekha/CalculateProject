122dc4748f2102bd85702599fa658654b5186dd1
Bhanu-lekha <bhanulekha2003@gmail.com>
2024-07-01 14:16:03 +0530

    Added a comment in Calculator.java Sum in HelloAll.java

--- CalculatorJavaProject/src/package1/HelloAll.java
+++ CalculatorJavaProject/src/package1/HelloAll.java
@@ -3,8 +3,11 @@ package package1;
 public class HelloAll {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello all");
+		int a=10;
+		int b=9;
+		System.out.println(a+b);
+		
 	}
 
 }
